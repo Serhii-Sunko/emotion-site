4ffec5d0d989a20533c13f6357dd4783c37e18ac
Serhii-Sunko <sergeysunko01@gmail.com>
2022-05-06 15:26:38 +0300

    do selector lang

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
-const english = {
-  title: "english",
-  guaranteeItems: {
-    maintitle: "AMP interactive becomes e’motion",
-    paragraph:
-      "The show will continue right here on this new web platform. Stay turned!",
+const langArr = {
+  title: {
+    en: "AMP interactive becomes e’motion",
+    fr: "AMP interactive devient e’motion",
   },
-};
-const français = {
-  title: "français",
-  guaranteeItems: {
-    maintitle: "AMP interactive devient e’motion",
-    paragraph:
-      "Le show va bientôt se poursuivre sur cette nouvelle plateforme web! Restez connectés.",
+  descr: {
+    en: "The show will continue right here on this new web platform. Stay turned!",
+    fr: "Le show va bientôt se poursuivre sur cette nouvelle plateforme web! Restez connectés.",
   },
 };
 
-const languages = {
-  english,
-  français,
-};
+// --------------------------Select Section --------------------------
 
-const currentLanguage = "english";
+const select = document.querySelector("select");
+const allLang = ["en", "fr"];
+select.addEventListener("change", changeURLLanguage);
+
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  // console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#fr";
+    location.reload();
+  }
+  select.value = hash;
+
+  for (let key in langArr) {
+    let elem = document.querySelector(".about_" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+  }
+}
+
+changeLanguage();
+const currentLanguage = window.location.hash.substr(1);
+console.log(currentLanguage);
 localStorage.setItem("language", JSON.stringify(currentLanguage));
-const links = [
-  {
-    name: languages[currentLanguage].guaranteeItems["maintitle"],
-    class: "about_title",
-  },
-  {
-    name: languages[currentLanguage].guaranteeItems["paragraph"],
-    class: "about_descr",
-  },
-];
-
-// const murkUp = links.find(item => item.name: languages[currentLanguage].guaranteeItems["maintitle"]);
-const murkUp = links.reduce((acc, item) => {
-  acc += `<div class="${item.class}">${item.name}</div>`;
-  return acc;
-}, "");
-
-document.querySelector(".about_info").innerHTML = murkUp;
